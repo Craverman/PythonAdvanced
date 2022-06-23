c43d5855c957051dc86b1324366e59ccbed670ff
Владислав Захаренков <Craverman>
2022-06-23 16:53:11 +0300

    Lesson 7. Creating sockets and chat

---
+++ Lesson7/client.py
@@ -0,0 +1,153 @@
+"""Программа-клиент"""
+
+import sys
+import json
+import socket
+import time
+import argparse
+import logging
+import logs.config_client_log
+from variables import DEFAULT_PORT, DEFAULT_IP_ADDRESS, \
+    ACTION, TIME, USER, ACCOUNT_NAME, SENDER, PRESENCE, RESPONSE, ERROR, MESSAGE, MESSAGE_TEXT
+from utils import get_message, send_message
+from decos import toLog
+from errors import ReqFieldMissingError, ServerError
+
+# Инициализация клиентского логера
+LOGGER = logging.getLogger('client')
+
+
+@toLog
+def message_from_server(message):
+    """Функция - обработчик сообщений других пользователей, поступающих с сервера"""
+    if ACTION in message and message[ACTION] == MESSAGE and \
+            SENDER in message and MESSAGE_TEXT in message:
+        print(f'Message from user '
+              f'{message[SENDER]}:\n{message[MESSAGE_TEXT]}')
+        LOGGER.info(f'Message from user '
+                    f'{message[SENDER]}:\n{message[MESSAGE_TEXT]}')
+    else:
+        LOGGER.error(f'Invalid message from server: {message}')
+
+
+@toLog
+def create_message(sock, account_name='Guest'):
+    """Функция запрашивает текст сообщения и возвращает его.
+    Так же завершает работу при вводе подобной комманды
+    """
+    message = input('Put your message here or \'BYE\' if you want to close chat:  ')
+    if message == 'BYE':
+        sock.close()
+        LOGGER.info('Chat closed')
+        print('Have a good day!')
+        sys.exit(0)
+    message_dict = {
+        ACTION: MESSAGE,
+        TIME: time.time(),
+        ACCOUNT_NAME: account_name,
+        MESSAGE_TEXT: message
+    }
+    LOGGER.debug(f'Dictionary with messages: {message_dict}')
+    return message_dict
+
+
+@toLog
+def create_presence(account_name='Guest'):
+    """Функция генерирует запрос о присутствии клиента"""
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    LOGGER.debug(f'{PRESENCE} message from user {account_name}')
+    return out
+
+
+@toLog
+def process_response_ans(message):
+    """
+    Функция разбирает ответ сервера на сообщение о присутствии,
+    возращает 200 если все ОК или генерирует исключение при ошибке
+    """
+    LOGGER.debug(f'Greetings from server: {message}')
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        elif message[RESPONSE] == 400:
+            raise ServerError(f'400 : {message[ERROR]}')
+    raise ReqFieldMissingError(RESPONSE)
+
+
+@toLog
+def arg_parser():
+    """Создаём парсер аргументов коммандной строки
+    и читаем параметры, возвращаем 3 параметра
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
+    parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-m', '--mode', default='listen', nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    server_address = namespace.addr
+    server_port = namespace.port
+    client_mode = namespace.mode
+
+    # проверим подходящий номер порта
+    if not 1023 < server_port < 65536:
+        LOGGER.critical(
+            f'Trying to connect to: {server_port}. '
+            f'Your server should be from 1024 to 65535.')
+        sys.exit(1)
+
+    return server_address, server_port, client_mode
+
+
+def main():
+    """Загружаем параметы коммандной строки"""
+    server_address, server_port, client_mode = arg_parser()
+
+    LOGGER.info(
+        f'Initializing client with ip adress: {server_address}, '
+        f'port: {server_port}, mode: {client_mode}')
+
+    # Инициализация сокета и сообщение серверу о нашем появлении
+    try:
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.connect((server_address, server_port))
+        send_message(transport, create_presence())
+        answer = process_response_ans(get_message(transport))
+        LOGGER.info(f'Connected to server: {answer}')
+        print(f'Connected to server')
+    except json.JSONDecodeError:
+        LOGGER.error('Unable to connect')
+        sys.exit(1)
+    else:
+        # Если соединение с сервером установлено корректно,
+        # начинаем обмен с ним, согласно требуемому режиму.
+        # основной цикл прогрммы:
+        if client_mode == 'send':
+            print('Current mode - send')
+        else:
+            print('Current mode - receive')
+        while True:
+            # режим работы - отправка сообщений
+            if client_mode == 'send':
+                try:
+                    send_message(transport, create_message(transport))
+                except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                    LOGGER.error(f'Connection with server {server_address} has been lost.')
+                    sys.exit(1)
+
+            # Режим работы приём:
+            if client_mode == 'listen':
+                try:
+                    message_from_server(get_message(transport))
+                except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                    LOGGER.error(f'Connectiomn to server {server_address} has been lost.')
+                    sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
